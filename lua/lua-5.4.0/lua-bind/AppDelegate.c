448f8d3f1e008271bc9ba52a16cb541db7dd6a9a
liujin1992112 <scliujin1992112@163.com>
2020-09-25 17:10:32 +0800

    测试cjson库再lua中集成效果

--- lua/lua-5.4.0/lua-bind/AppDelegate.c
+++ lua/lua-5.4.0/lua-bind/AppDelegate.c
@@ -7,7 +7,12 @@ int main()
 	luaL_openlibs(L);
 
 	luaopen_lua_extensions_more(L);
-	luaL_dofile(L, "main.lua");
+	int nRet = luaL_dofile(L, "main.lua");
+	if (nRet != LUA_OK)
+	{
+		const char *err = lua_tostring(L, -1); //加载失败，会把错误信息压入栈顶
+		printf("-------loadfile error = %s\n", err);
+	}
 	lua_close(L);
 
 	system("pause");
--- lua/lua-5.4.0/lua-bind/main.lua
+++ lua/lua-5.4.0/lua-bind/main.lua
@@ -1,5 +1,31 @@
-json = require("cjson")
+json = require("cjson").new()
 print("闭眼就天黑")
 print(json)
-local str = "{id:1,name="刘金"}"
-print(json.decode(str))
+
+local lua_bool = true
+print(json.encode(lua_bool))
+
+-- 数组类型
+local lua_array = {1, 2, 3, 4, 5, 6}
+print(json.encode(lua_array))
+
+-- 数值类型
+local lua_number = 6.66
+print(json.encode(lua_number))
+
+-- 字符串类型
+local lua_string = "I am test1280"
+print(json.encode(lua_string))
+
+-- 对象类型
+local lua_object = {
+    ["name"] = "Jiang",
+    ["age"] = 24,
+    ["addr"] = "BeiJing",
+    ["email"] = "1569989xxxx@126.com",
+    ["tel"] = "1569989xxxx"
+}
+
+local jsonStr = json.encode(lua_object)
+print(jsonStr)
+print(json.decode(jsonStr))
